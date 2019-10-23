3048baa07e041261b3f49af2cc6e92c5e72bb5fa
Joaquin Gutierrez <joa18gm@gmail.com>
2019-10-23 08:25:32 -0500

    actualizacion de productos y precios

--- Software.py
+++ Software.py
@@ -1,7 +1,9 @@
 #Definir productos y precios
 
-productos = ["arrozlacosteña", "cocacola", "inkakola", "chocolatesorrento", "snickers", "papaslays", "pringles", 
+productos = ["arrozcosteño", "cocacolapersonal", "inkakolapersonal", "chocolatesorrento", "snickers", "papaslays", "pringles", 
 "detergenteariel", "galletascasino", "galletaschaplin", "chocolatevicio", "ajinosillao", "ketchupalacena", 
 "cubosmaggie", "ajinomen"]
-precios = []
-stock = [20, 15, 15 ,16,12,25,25,10, 14, 5, 10 ,15, 10,     ]
+precios = [2.50,2.00,1.50,2.50,3.50,1.50,5.00,3.50,1.20,1.00,2.00,7.00,4.00,3.50,2.50]
+stock = [20,15,15,16,12,25,25,10,14,5,10,15,10,20,50]
+
+
