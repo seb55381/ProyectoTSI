aa2f3e2d3a2d00a9ddc11eb039be2411133ea564
Adrian1608 <eladrian1608@gmail.com>
2019-10-23 22:26:05 -0500

    Error arreglado y condicionales hechas uwu

--- Software.py
+++ Software.py
@@ -8,22 +8,31 @@ productos = ["arrozcosteño", "cocacolapersonal", "inkakolapersonal", "chocolate
 "cubosmaggie", "ajinomen"]
 precios = [2.50,2.00,1.50,2.50,3.50,1.50,5.00,3.50,1.20,1.00,2.00,7.00,4.00,3.50,2.50]
 stock = [20,15,15,16,12,25,25,10,14,5,10,15,10,20,50]
-
+comprados = []
 
 # Cuando el cajero digita los productos:
 
-i = 0
+i = 1
 while True:
-    i+=1
     entrada=input("Ingrese el producto " + str(i) + " : ")
     entrada = entrada.lower()
-    for p in productos:
+    entrada = entrada.replace(" ","")
+    for p in range(0, len(productos)):
         if (entrada == ""):
-            print("Ingrese un producto ")
+            print("Tiene que ingresar un producto.")
             break
-        elif entrada != :
-            print("El producto ha sido introducido erroneamente")
+        elif entrada == productos[p]:
+            i += 1
+            comprados.append(entrada)
             break
-    break
+    if comprados.count(entrada) == 0:
+        print("El producto ha sido insertado incorrectamente.")
+    else:
+        elbooleano = input("¿Insertar más productos a la boleta?(Sí/No): ")
+        elbooleano = elbooleano.lower()
+        elbooleano = elbooleano.replace("í", "i")
+        elbooleano = elbooleano.replace(" ","")
+        if elbooleano == "no":
+            break
 
-print(entrada)
+print(comprados)
